92c0c3daf992d38f93044828fd0d3c6492331144
Py-Ashraful <ashrafulislam180@gmail.com>
2021-09-24 22:50:54 +0600

    challenge

--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -6,24 +6,24 @@
  * Execute: Use `npx jest --watch src/no-framework/mock-fn.js` to watch the test
  */
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
 // Your Code:
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = fn((p1, p2) => p1)
+const originalGetWinner = utils.getWinner;
+utils.getWinner = fn((p1, p2) => p1);
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler'],
-])
+  ["Kent C. Dodds", "Ken Wheeler"],
+  ["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
-utils.getWinner = originalGetWinner
+utils.getWinner = originalGetWinner;
 
 /**
  * Checkout master branch to see the answer.
